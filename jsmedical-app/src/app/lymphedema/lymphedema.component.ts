62ffdb631d1cecf0f2b3691c3fe9329736328edb
Don Miko <dmiko1994@gmail.com>
2021-03-24 22:56:35 -0400

    lymphedema work, patient-ed jpg, styling

--- jsmedical-app/src/app/lymphedema/lymphedema.component.ts
+++ jsmedical-app/src/app/lymphedema/lymphedema.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lymphedema',
   templateUrl: './lymphedema.component.html',
-  styleUrls: ['./lymphedema.component.css']
+  styleUrls: ['./lymphedema.component.scss']
 })
 export class LymphedemaComponent implements OnInit {
 
