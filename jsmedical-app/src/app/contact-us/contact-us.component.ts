4b24b208004cf7be431e0ebf56d5ebf8e2d7fc7a
Don Miko <djm1994@gmail.com>
2021-04-06 20:03:23 -0400

    Styling

--- jsmedical-app/src/app/contact-us/contact-us.component.ts
+++ jsmedical-app/src/app/contact-us/contact-us.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+
+  emailString = "mailto:jsmedical@gmail.com?Subject=JSMedical Inquiry";
 
   constructor() { }
 
